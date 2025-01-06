b20d01fa65cde359d50a4ada066661b8e29f9483
Trishank-K <itstrishank@gmail.com>
2025-01-06 06:12:51 +0530

    Revamp Landing Page

---
+++ src/components/site/navigation/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {auth, User} from "@clerk/nextjs/server"
+import Image from 'next/image'
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+import {ModeToggle} from "@/components/global/mode-toggle"
+
+type Props = {
+    user?:null | User
+}
+
+const Navigation = (props: Props) =>    {
+    const user = auth();
+    console.log("User",user)
+    return <div className="p-4 flex items-center justify-between relative">
+        
+        <aside className="flex items-center gap-2">
+            <Image className=""
+            src={"/assets/Logo.png"}
+            alt="Logo"
+            width={40}
+            height={40}
+            />
+            <span className="text-xl font-bold">Websy</span>
+        </aside>
+        <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
+            <ul className="flex items-center justify-center gap-8">
+                <Link href={'#'}>Pricing</Link>
+                <Link href={'#'}>About</Link>
+                <Link href={'#'}>Documentation</Link>
+                <Link href={'#'}>Features</Link>
+            </ul>
+        </nav>
+        <aside className="flex gap-2 items-center">
+                <Link href={user.userId?'/agency/':'/agency/sign-in'} className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80">{user.userId ? 'Go to Agency':'Login'}</Link>
+                <UserButton/>
+                <ModeToggle/>
+        </aside>
+    </div>
+}
+export default Navigation
